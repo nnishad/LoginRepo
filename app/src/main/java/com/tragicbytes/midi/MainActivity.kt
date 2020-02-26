0ad53bf8f7470fdbb6b410f9fcb799a5ea709c61
Anmol <anmol.agarwal2004@yahoo.com>
2020-02-27 03:05:31 +0530

    Update Module Added with firebase Config

--- app/src/main/java/com/tragicbytes/midi/MainActivity.kt
+++ app/src/main/java/com/tragicbytes/midi/MainActivity.kt
@@ -1,12 +1,107 @@
 package com.tragicbytes.midi
 
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.DialogInterface
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : FirebaseConfig() {
+    private lateinit var mFirebaseRemoteConfig: FirebaseRemoteConfig
+    private var VersionCode = "versionCode"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mFirebaseRemoteConfig = getRemoteConfigValues()
+        setRemoteConfigValues()
     }
+    //region Firebase Config Method 2
+    private fun setRemoteConfigValues() {
+        //region Fetching Values
+        val remoteCodeVersion = mFirebaseRemoteConfig.getLong(VersionCode)
+        val AlertTitle = mFirebaseRemoteConfig.getString("Alert_Title")
+        val AlertMessage = mFirebaseRemoteConfig.getString("Alert_Message")
+        val Alert_Ok_btn = mFirebaseRemoteConfig.getString("Alert_Ok_Btn")
+        val Alert_No_btn = mFirebaseRemoteConfig.getString("Alert_No_Btn")
+
+        //endregion
+
+        if (remoteCodeVersion > 0) {
+            val versionCode = BuildConfig.VERSION_CODE
+            if (remoteCodeVersion > versionCode) {
+                val dialogBuilder = AlertDialog.Builder(this)
+
+
+                // set message of alert dialog
+                dialogBuilder.setMessage(AlertMessage)
+                    // if the dialog is cancelable
+                    .setCancelable(false)
+                    // positive button text and action
+                    .setPositiveButton(Alert_Ok_btn, DialogInterface.OnClickListener { _, _ ->
+                        val uri = Uri.parse("market://details?id=" + this@MainActivity.packageName)
+                        val goToMarket = Intent(Intent.ACTION_VIEW, uri)
+                        // To count with Play market backstack, After pressing back button,
+                        // to taken back to our application, we need to add following flags to intent.
+                        goToMarket.addFlags(
+                            Intent.FLAG_ACTIVITY_NO_HISTORY or
+                                    Intent.FLAG_ACTIVITY_NEW_DOCUMENT or
+                                    Intent.FLAG_ACTIVITY_MULTIPLE_TASK
+                        )
+                        try {
+                            startActivity(goToMarket)
+                        } catch (e: ActivityNotFoundException) {
+                            startActivity(
+                                Intent(
+                                    Intent.ACTION_VIEW,
+                                    Uri.parse("http://play.google.com/store/apps/details?id=" + this@MainActivity.packageName)
+                                )
+                            )
+                        }
+                    })
+                    // negative button text and action
+                    .setNegativeButton(Alert_No_btn, // do something when the button is clicked
+                        DialogInterface.OnClickListener { _, _ ->
+                            finishAffinity()
+                        })
+
+
+                // create dialog box
+                val alert = dialogBuilder.create()
+                // set title for alert dialog box
+                alert.setTitle(AlertTitle)
+                // show alert dialog
+                alert.show()
+            }
+            //  main_layout!!.setBackgroundColor(Color.parseColor(remoteValueText))
+        }
+
+    }
+    //endregion
+
+    //region Firebase Config Method 3
+    override fun onStart() {
+
+        super.onStart()
+        mFirebaseRemoteConfig = getRemoteConfigValues()
+        //region Startup Notification Firebase Config
+        val remoteCodeVersion = mFirebaseRemoteConfig.getLong(VersionCode)
+        val versionCode = BuildConfig.VERSION_CODE
+
+        if (remoteCodeVersion > versionCode) {
+            getRemoteConfigValues()
+        }
+        //endregion
+    }
+    //endregion
+
+    //region Back Press
+    override fun onBackPressed() {
+        finishAffinity()
+        finish()
+    }
+    //endregion
 }
